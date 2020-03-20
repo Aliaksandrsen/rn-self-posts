75a38847221492d94f7dff0da15bd03d4833242c
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-03-20 14:29:36 +0300

    feat: adaptation for android

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,32 +1,30 @@
 import React from 'react'
-import { createAppContainer } from 'react-navigation'
+import { createAppContainer, ThemeColors } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 import { Platform } from 'react-native'
-import { Ionicons } from '@expo/vector-icons';
-
+import { Ionicons } from '@expo/vector-icons'
 import { MainScreen } from '../screens/MainScreen'
 import { PostScreen } from '../screens/PostScreen'
 import { BookedScreen } from '../screens/BookedScreen'
-
 import { THEME } from '../theem'
+
+const navigatorOptions = {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
+    },
+    headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR
+  }
+}
 
 const PostNavigator = createStackNavigator(
   {
     Main: MainScreen,
-    Post: {
-      screen: PostScreen
-    }
+    Post: PostScreen
   },
-  {
-    initialRouteName: 'Main', // главный экран
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
-      },
-      headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
-    }
-  }
+  navigatorOptions
 )
 
 const BookedNavigator = createStackNavigator(
@@ -34,37 +32,43 @@ const BookedNavigator = createStackNavigator(
     Booked: BookedScreen,
     Post: PostScreen
   },
-  {
-    initialRouteName: 'Booked', // можно не писать будет первый ключ
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
-      },
-      headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
-    }
-  }
+  navigatorOptions
 )
 
-const BottomNavigator = createBottomTabNavigator({
+const bottomTabsConfig = {
   Post: {
     screen: PostNavigator,
     navigationOptions: {
-      // ф-я необх для передачи цвета
-      tabBarIcon: info => <Ionicons name='ios-albums' size={25} color={info.tintColor}/>
+      tabBarLabel: 'Все',
+      tabBarIcon: info => (
+        <Ionicons name='ios-albums' size={25} color={info.tintColor} />
+      )
     }
   },
   Booked: {
     screen: BookedNavigator,
     navigationOptions: {
-      tabBarIcon: info => <Ionicons name='ios-star' size={25} color={info.tintColor}/>
+      tabBarLabel: 'Избранное',
+      tabBarIcon: info => (
+        <Ionicons name='ios-star' size={25} color={info.tintColor} />
+      )
     }
   }
-},
-// options createBottomTabNavigator
-{
-  tabBarOptions: {
-    activeTintColor: THEME.MAIN_COLOR
-  }
-})
+}
+
+const BottomNavigator =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(bottomTabsConfig, {
+        activeTintColor: '#fff',
+        shifting: true, // необяз параметр + текст под иконкой
+        barStyle: {
+          backgroundColor: THEME.MAIN_COLOR
+        }
+      })
+    : createBottomTabNavigator(bottomTabsConfig, {
+        tabBarOptions: {
+          activeTintColor: THEME.MAIN_COLOR
+        }
+      })
 
 export const AppNavigation = createAppContainer(BottomNavigator)
